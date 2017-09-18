4c34be2efec54a08fe9bc77c0d93d8485768282e
yTakkar <www.shtakkar@gmail.com>
2017-09-18 20:14:50 +0530

    Updated main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,18 @@
-puts "Hello, World!!"
+class Name 
+
+  @@d = "Hello, World!!"
+
+  def hello_to_world
+    @d = "Hello, India!!"
+    puts @@d
+  end
+
+  def hello_to_india
+    puts @d
+  end
+
+end
+
+n = Name.new
+n.hello_to_world
+n.hello_to_india
